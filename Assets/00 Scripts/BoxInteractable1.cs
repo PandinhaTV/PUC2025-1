723e0e0af7508b1677f33b2039ece4c55ca1ea98
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-01-21 04:54:49 +0000

    update

--- Assets/00 Scripts/BoxInteractable1.cs
+++ Assets/00 Scripts/BoxInteractable1.cs
@@ -9,8 +9,13 @@ public class BoxInteractable1 : MonoBehaviour, IInteractable
     public string prompttext = "Next Level (E)";
     public void Interact(GameObject interactor)
     {
+        SceneController.Instance
+            .NewTransition()
+            .Load(SceneDatabase.Slots.Session, SceneDatabase.Scenes.Corridor1)
+            .WithOverlay()
+            .Perform();
         Debug.Log($"{interactor.name} opened {doorName}!");
-        Application.Quit();
+        
         // Add your door animation, sound, or logic here
     }
 }
--- Assets/00 Scripts/SceneController/SceneDatabase.cs
+++ Assets/00 Scripts/SceneController/SceneDatabase.cs
@@ -14,10 +14,10 @@ public static class SceneDatabase
         public const string MainMenu = "MainMenu";
         public const string Session = "Session";
         public const string Test = "SampleScene";
-        public const string BuildingTest = "BuildingTest";
-        public const string City = "City 2";
-        public const string Match = "Match";
-        public const string Login = "Login";
+        public const string Room1 = "Room new";
+        public const string Room2 = "Room2";
+        public const string Corridor1 = "Corridor1";
+        public const string Corridor2 = "Corridor2";
         public const string Profile = "Profile";
         public const string Bairro = "Bairro";
     }
