fa3b821267fac80493d81537e9dad804d1a45845
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-02-01 20:49:31 +0000

    mm,

--- Assets/00 Scripts/SceneController/SceneDatabase.cs
+++ Assets/00 Scripts/SceneController/SceneDatabase.cs
@@ -16,7 +16,7 @@ public static class SceneDatabase
         public const string Test = "SampleScene";
         public const string Room1 = "Room new";
         public const string Room2 = "Room2";
-        public const string Room3 = "Room3";
+        public const string Room3 = "Room3 1";
         public const string Room4 = "Room4";
         public const string Corridor1 = "Corridor_1";
         public const string Corridor2 = "Corridor_2";
